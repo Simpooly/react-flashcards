2893ed853722089c2bf70ad1d2d0f32e376679e4
Krista <krista.t.jekel@gmail.com>
2018-06-19 15:41:10 -0600

    Get Values from Form Input

--- src/Form/Form.jsx
+++ src/Form/Form.jsx
@@ -13,19 +13,31 @@ class Form extends Component{
 		this.props.close();
 	}
 
+	onChange (){ console.log('Form works!');}
+
+	onSubmit(e){
+	      e.preventDefault();
+
+	      {/* gets value from input field */}
+	      var textFront = this.refs.front.value.trim();
+	      var textBack = this.refs.back.value.trim();
+
+	      console.log(textFront, textBack);
+}
+
 	render(props){
 		return(
 			<div id="myModal" className="modal">
 				<div className="modal-content">
 				<span className="close" onClick={this.close}>&times;</span>
-					<form>
+					<form onSubmit={this.onSubmit.bind(this)}>
 						<div className="form-group">
 					    	<label >Front</label>
-					    	<textarea className="form-control" id="exampleFormControlTextarea1" rows="1"></textarea>
+					    	<textarea type="text" ref="front" onChange={this.onChange} className="form-control" id="exampleFormControlTextarea1" rows="1"></textarea>
 						</div>
 					 	<div className="form-group">
 							<label >Back</label>
-							<textarea className="form-control" id="exampleFormControlTextarea1" rows="4"></textarea>
+							<textarea type="text" ref="back" className="form-control" id="exampleFormControlTextarea1" rows="4"></textarea>
 						</div>
 						<button type="submit" >Submit</button>
 					</form>
